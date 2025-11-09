3831170fec7415caa7c0843ae716fb8293fa5c33
David Fleming <tonage72@gmail.com>
2025-11-09 14:34:03 -0500

    Initial commit

---
+++ entry.py
@@ -0,0 +1,18 @@
+from tkinter import *
+
+root = Tk()
+
+e = Entry(root, width=50, borderwidth=5)
+e.pack()
+e.insert(0, "Enter your name: ")
+
+def myClick():
+	hello = "Hello " + e.get()
+	myLabel = Label(root, text=hello)
+	myLabel.pack()
+
+myButton = Button(root, text="Enter your name", command=myClick, fg="blue", bg="red")
+
+myButton.pack()
+
+root.mainloop()
